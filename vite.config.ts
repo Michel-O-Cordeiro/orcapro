4cea150dbcb498eb4aea7f46fd0d013284c4bd8d
Michel-O-Cordeiro <cadastromichel@gmail.com>
2026-06-01 18:01:03 -0300

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1200,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
